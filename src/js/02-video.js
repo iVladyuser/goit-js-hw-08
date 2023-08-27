5e44edab935c17a263ee08b76e77bb129a6b7c5a
iVladyuser <korol.v09@gmail.com>
2023-08-27 18:32:56 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// const player = new Player('handstick', {});
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+
+const currentSeconds = localStorage.getItem('videoplayer-current-time') || 0;
+
+iframePlayer.setCurrentTime(currentSeconds);
